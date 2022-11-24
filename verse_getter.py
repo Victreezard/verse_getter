6ef8653609c517f85eea89827266ce0874e99e12
Victreezard <14161440+Victreezard@users.noreply.github.com>
2022-11-24 18:07:50 +0800

    Update verse_getter.py
    Refactor code

--- verse_getter.py
+++ verse_getter.py
@@ -1,7 +1,8 @@
+from time import perf_counter
 import bible_data as bible
 import obspython as obs
 import re
-import urllib.parse
+import urllib.error
 import urllib.request
 
 base_url = 'https://ibibles.net/quote.php?'
@@ -14,16 +15,16 @@ def extract_verse(data: str) -> str:
     return re.findall("(?<=</small>).+(?=<br>)", data)[0].strip()
 
 
-def update_source(bible_verse: str, url: str):
+def update_source(bible_verse: str, url: str) -> str:
     """Get the verse from the Bible Display API and update the Text Source"""
     source = obs.obs_get_source_by_name(source_name)
-    if source is not None:
+    text = ""
+    if source:
         try:
             # Call API
-            print(f"URL: {url}")
             with urllib.request.urlopen(url) as response:
                 data = response.read().decode('utf-8')
-                print(data)
+                print(f"{data=}")
                 text = bible_verse + "\n" + extract_verse(data)
                 # Update Text Source
                 settings = obs.obs_data_create()
@@ -38,8 +39,10 @@ def update_source(bible_verse: str, url: str):
 
         obs.obs_source_release(source)
 
+    return text
 
-def get_verse(op=None):
+
+def get_verse(op=None) -> str:
     """
     Build the bible verse format and the complete URL for the API call
     op defines whether to increment (if True) or decrement (if False) the verse number
@@ -67,27 +70,46 @@ def get_verse(op=None):
 
     bible_verse = f"{ver.upper()} {book.title()} {ch_verse}"
     url = f"{base_url}{ver}-{bk}/{ch_verse}"
-    update_source(bible_verse, url)
+    print(f"{url=}")
+    return update_source(bible_verse, url)
 
 
 def get_pressed(props, prop):
-    get_verse()
+    time_start = perf_counter()
+
+    verse = get_verse()
+
+    time_stop = perf_counter()
+    time_elapsed = str(time_stop-time_start)
+    obs.obs_data_set_string(settings_copy, "info_text",
+                            formatted_time := f"{time_elapsed=!s}")
+    obs.obs_data_set_string(settings_copy, "result_text", verse)
+    print(formatted_time)
+
+    return True
 
 
 def next_pressed(props, prop):
-    get_verse(op=True)
+    verse = get_verse(op=True)
+    obs.obs_data_set_string(settings_copy, "chapter_verse", chapter_verse)
+    obs.obs_data_set_string(settings_copy, "result_text", verse)
+
+    return True
 
 
 def prev_pressed(props, prop):
-    get_verse(op=False)
+    verse = get_verse(op=False)
+    obs.obs_data_set_string(settings_copy, "chapter_verse", chapter_verse)
+    obs.obs_data_set_string(settings_copy, "result_text", verse)
+
+    return True
+
 
 # ---------------Script Global Functions---------------
 
 
 def script_description():
-    return """Verse Getter
-Note: Chapter & Verse does not change when Next or Prev button is pressed."""
-
+    return """Verse Getter"""
 
 def script_defaults(settings):
     """Sets the default values of the Script's properties when 'Defaults' button is pressed"""
@@ -100,14 +122,16 @@ def script_defaults(settings):
 
 def script_update(settings):
     global source_name
-    global version
-    global book
-    global chapter_verse
-
     source_name = obs.obs_data_get_string(settings, "source")
+    global version
     version = obs.obs_data_get_string(settings, "version")
+    global book
     book = obs.obs_data_get_string(settings, "book")
+    global chapter_verse
     chapter_verse = obs.obs_data_get_string(settings, "chapter_verse")
+
+    global settings_copy
+    settings_copy = settings
 
 
 def script_properties():
@@ -116,12 +140,13 @@ def script_properties():
     p = obs.obs_properties_add_list(props, "source", "Text Source",
                                     obs.OBS_COMBO_TYPE_EDITABLE, obs.OBS_COMBO_FORMAT_STRING)
     sources = obs.obs_enum_sources()
-    if sources is not None:
+    if sources:
         for source in sources:
             source_id = obs.obs_source_get_unversioned_id(source)
-            if source_id == "text_gdiplus" or source_id == "text_ft2_source":
-                name = obs.obs_source_get_name(source)
-                obs.obs_property_list_add_string(p, name, name)
+            match source_id:
+                case "text_gdiplus" | "text_ft2_source":
+                    name = obs.obs_source_get_name(source)
+                    obs.obs_property_list_add_string(p, name, name)
 
         obs.source_list_release(sources)
     # Version List
@@ -144,5 +169,8 @@ def script_properties():
         props, "next_button", "Next Verse", next_pressed)
     obs.obs_properties_add_button(
         props, "prev_button", "Previous Verse", prev_pressed)
+
+    obs.obs_properties_add_text(props, "info_text", "", obs.OBS_TEXT_INFO)
+    obs.obs_properties_add_text(props, "result_text", "Result", obs.OBS_TEXT_MULTILINE)
 
     return props
