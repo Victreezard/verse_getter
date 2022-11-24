3ab7bb0bfe14aca613ec7e26f9402c8b4076159b
Victreezard <14161440+Victreezard@users.noreply.github.com>
2022-11-24 18:06:31 +0800

    Update and rename verse_of_the_day.py to verse_otd.py
    Refactor code

--- verse_otd.py
+++ verse_otd.py
@@ -1,7 +1,8 @@
+from time import perf_counter
 import bible_data as bible
 import obspython as obs
 import re
-import urllib.parse
+import urllib.error
 import urllib.request
 
 base_url = "https://www.biblegateway.com/usage/votd/rss/votd.rdf?$31"
@@ -11,15 +12,21 @@ base_url = "https://www.biblegateway.com/usage/votd/rss/votd.rdf?$31"
 
 def extract_bible_verse(data: str) -> str:
     """Extract the verse from the HTML body"""
-    results = re.findall(r"(?<=<title>).+:.+(?=</title>)|(?<=&ldquo;).+(?=&rdquo;)", data)
-    # print(results) # Print filtered Title and Verse
-    return results[1] + "\n\n" + results[0]
+    results = re.findall(
+        r"(?<=<title>).+:.+(?=</title>)|(?<=&ldquo;).+(?=&rdquo;)", data)
+    # print(f"{results=}")
+    title = results[0]
+    verse = re.sub(r" {2,}", " ", results[1])
+    verse = re.sub(r"\[[\w ]+\] ", "", verse)
+    # print(f"{title=}")
+    # print(f"{verse=}")
+    return verse + "\n\n" + title
 
 
-def update_source(data: str):
+def update_source(data: str) -> None:
     """Update the Text Source"""
     source = obs.obs_get_source_by_name(source_name)
-    if source is not None:
+    if source:
         try:
             settings = obs.obs_data_create()
             obs.obs_data_set_string(settings, "text", data)
@@ -39,29 +46,40 @@ def get_verse() -> str:
     data = ""
     try:
         # Call API
-        # print(f"URL: {base_url}")
+        # print(f"{base_url=}")
         with urllib.request.urlopen(base_url) as response:
             data = response.read().decode('utf-8')
-            # print(data) # Print raw data
+            # print(f"{data=}")
             data = extract_bible_verse(data)
 
     except urllib.error.URLError as err:
         obs.script_log(obs.LOG_WARNING,
-                        "Error opening URL '" + base_url + "': " + err.reason)
+                       "Error opening URL '" + base_url + "': " + err.reason)
         obs.remove_current_callback()
 
     return data
 
 
-def get_pressed(props, prop):
-    update_source( get_verse() )
+def activate_callback(props, prop):
+    time_start = perf_counter()
+
+    update_source(get_verse())
+
+    time_stop = perf_counter()
+    time_elapsed = str(time_stop-time_start)
+    obs.obs_data_set_string(settings_copy, "info_text",
+                            formatted_time := f"{time_elapsed=!s}")
+    print(formatted_time)
+
+    return True
 
 
 # ---------------Script Global Functions---------------
 
 
 def script_description():
-    return """Verse of the Day - BibleGateway RSS"""
+    return """Verse of the Day - BibleGateway RSS
+TODO: Add other bible verse sources"""
 
 
 def script_defaults(settings):
@@ -73,8 +91,9 @@ def script_defaults(settings):
 
 def script_update(settings):
     global source_name
-
     source_name = obs.obs_data_get_string(settings, "source")
+    global settings_copy
+    settings_copy = settings
 
 
 def script_properties():
@@ -83,12 +102,13 @@ def script_properties():
     p = obs.obs_properties_add_list(props, "source", "Text Source",
                                     obs.OBS_COMBO_TYPE_EDITABLE, obs.OBS_COMBO_FORMAT_STRING)
     sources = obs.obs_enum_sources()
-    if sources is not None:
+    if sources:
         for source in sources:
             source_id = obs.obs_source_get_unversioned_id(source)
-            if source_id == "text_gdiplus" or source_id == "text_ft2_source":
-                name = obs.obs_source_get_name(source)
-                obs.obs_property_list_add_string(p, name, name)
+            match source_id:
+                case "text_gdiplus" | "text_ft2_source":
+                    name = obs.obs_source_get_name(source)
+                    obs.obs_property_list_add_string(p, name, name)
 
         obs.source_list_release(sources)
     # Version List
@@ -98,6 +118,8 @@ def script_properties():
         obs.obs_property_list_add_string(p, ver, bible.VERSION.get(ver))
     # Action Buttons
     obs.obs_properties_add_button(
-        props, "get_button", "Get Verse", get_pressed)
+        props, "get_button", "Get Verse", activate_callback)
+
+    obs.obs_properties_add_text(props, "info_text", "", obs.OBS_TEXT_INFO)
 
     return props
